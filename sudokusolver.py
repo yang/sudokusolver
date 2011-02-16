fcdecefe03948b2bac10c2254ddb849435d3f736
Yang Zhang <yaaang@gmail.com>
2011-02-16 14:45:04 -0800

    Clean up code

--- sudokusolver.py
+++ sudokusolver.py
@@ -1,5 +1,3 @@
-import copy
-
 input='''
 159     3
     8   6
@@ -45,40 +43,38 @@ dims = [(r,c) for r in xrange(9) for c in xrange(9)]
 def Set(xs): return set(x for x in xs if x is not None)
 grps=[[(r,c) for r in range(g/3*3, g/3*3+3) for c in range(g%3*3, g%3*3+3)] for g in xrange(9)]
 grpof=dict(((r,c), r/3*3+c/3) for r,c in dims)
-while 1:
-  mat2 = copy.deepcopy(mat)
-  def refresh():
-    inrow=[Set(mat[r][c] for c in dim) for r in dim]
-    incol=[Set(mat[r][c] for r in dim) for c in dim]
-    ingrp=[Set(mat[r][c] for r,c in grp) for grp in grps]
-    return inrow, incol, ingrp
-  inrow, incol, ingrp = refresh()
 
+def refresh():
+  inrow=[Set(mat[r][c] for c in dim) for r in dim]
+  incol=[Set(mat[r][c] for r in dim) for c in dim]
+  ingrp=[Set(mat[r][c] for r,c in grp) for grp in grps]
+  return inrow, incol, ingrp
+
+def update(r,c,n):
+  mat[r][c] = n
+  incol[c].add(n)
+  inrow[r].add(n)
+  ingrp[grpof[r,c]].add(n)
+
+inrow, incol, ingrp = refresh()
+while 1:
+  def do(spots, label):
+    updated = False
+    for n in nums:
+      poss = [(r,c) for r,c in spots if not mat[r][c] and n not in inrow[r] | incol[c] | ingrp[grpof[r,c]]]
+      if len(poss)==1:
+        print 'the %s in %s can go in %s' % (n,label,poss)
+        [(r,c)] = poss
+        update(r,c,n)
+        updated |= True
+    return updated
+  updated = False
   for r in dim:
-    for n in nums:
-      poss = [(r,c) for c in dim if not mat[r][c] and n not in inrow[r] | incol[c] | ingrp[grpof[r,c]]]
-      if len(poss)==1:
-        print 'for row %s, %s can go in %s' % (r,n,poss)
-        [(r,c)] = poss
-        mat[r][c] = n
-        inrow, incol, ingrp = refresh()
+    updated |= do([(r,c) for c in dim], 'row %s' % r)
   for c in dim:
-    for n in nums:
-      poss = [(r,c) for r in dim if not mat[r][c] and n not in inrow[r] | incol[c] | ingrp[grpof[r,c]]]
-      if len(poss)==1:
-        print 'for col %s, %s can go in %s' % (c,n,poss)
-        [(r,c)] = poss
-        mat[r][c] = n
-        inrow, incol, ingrp = refresh()
+    updated |= do([(r,c) for r in dim], 'col %s' % c)
   for g, grp in enumerate(grps):
-    for n in nums:
-      poss = [(r,c) for r,c in grp if not mat[r][c] and n not in incol[c] | inrow[r] | ingrp[grpof[r,c]]]
-      if len(poss)==1:
-        print 'for grp %s, %s can go in %s' % (g,n,poss)
-        if n == 9: import pdb; pdb.set_trace()
-        [(r,c)] = poss
-        mat[r][c] = n
-        inrow, incol, ingrp = refresh()
-  if mat2 == mat: break
+    updated |= do(grp, 'grp %s' % g)
+  if not updated: break
 
 # vim: et sw=2 ts=2
