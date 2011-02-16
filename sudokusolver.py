f2168cc5b944dd3bdca9f54d74b33edac6ef0b1b
Yang Zhang <yaaang@gmail.com>
2011-02-16 14:47:36 -0800

    Improve performance based on profiling

--- sudokusolver.py
+++ sudokusolver.py
@@ -45,47 +45,44 @@ dims = [(r,c) for r in xrange(9) for c in xrange(9)]
 
 def Set(xs): return set(x for x in xs if x is not None)
 grps=[[(r,c) for r in range(g/3*3, g/3*3+3) for c in range(g%3*3, g%3*3+3)] for g in xrange(9)]
-grpof=dict(((r,c), r/3*3+c/3) for r,c in dims)
+def grpof(r,c): return r/3*3+c/3
+taken=set(xrange(1,10))
 
-def update(r,c,n):
-  mat[r][c] = n
-  incol[c].add(n)
-  inrow[r].add(n)
-  ingrp[grpof[r,c]].add(n)
-
-def take(n, xs):
-  for i in xrange(n):
-    yield xs.next()
-
-def rec(mat, opt, depth, sure):
-  def indent(): return ' '*(2*depth-1)
-  mat = copy.deepcopy(mat)
-  if opt is not None:
-    print indent(), 'trying', opt, 'depth', depth, 'sure' if sure else ''
-    r,c,n = opt
-    mat[r][c] = n
-    if depth == 55:
-       print '\n'.join(''.join(' ' if c is None else str(c) for c in line) for line in mat)
-  if all(all(row) for row in mat): return [mat]
-  inrow=[Set(mat[r][c] for c in dim) for r in dim]
-  incol=[Set(mat[r][c] for r in dim) for c in dim]
-  ingrp=[Set(mat[r][c] for r,c in grp) for grp in grps]
-  def Gen():
+debug=0
+from cProfile import *
+inrow=[Set(mat[r][c] for c in dim) for r in dim]
+incol=[Set(mat[r][c] for r in dim) for c in dim]
+ingrp=[Set(mat[r][c] for r,c in grp) for grp in grps]
+def rec(nfilled, sure):
+  if nfilled == 9**2: return copy.deepcopy(mat)
+  def indent(): return ' '*(2*(nfilled-nfilled0)-1)
+  incell=[[taken if mat[r][c] else inrow[r] | incol[c] | ingrp[grpof(r,c)] for c in dim] for r in dim]
+  def genopts():
     for n in nums:
       def do(gen, label):
-        return [(r,c,n) for r,c in gen if not mat[r][c] and n not in inrow[r] | incol[c] | ingrp[grpof[r,c]]]
+        return [(r,c,n) for r,c in gen if n not in incell[r][c]]
       yield (do(((r,c) for c in dim), 'row %s' % r) for r in dim if n not in inrow[r])
       yield (do(((r,c) for r in dim), 'col %s' % c) for c in dim if n not in incol[c])
       yield (do(grp, 'grp %s' % g) for g, grp in enumerate(grps) if n not in ingrp[g])
-  opts = filter(lambda opts: opts != [], chain.from_iterable(Gen()))
-  if opts == []:
-    return []
-  else:
-    bestopts = min(opts, key=len)
-    if bestopts == []:
-      return []
-    if len(bestopts) > 1: print indent(), bestopts
-    return islice(chain.from_iterable(rec(mat, opt, depth+1, sure and len(bestopts)==1) for opt in bestopts), 1)
-print list(rec(mat, None, 0, True))
+  opts = filter(lambda opts: opts != [], chain.from_iterable(genopts()))
+  if opts == []: return None # when does this happen?
+  bestopts = min(opts, key=len)
+  if bestopts == []: return None
+  if debug and len(bestopts) > 1: print indent(), bestopts
+  for r,c,n in bestopts:
+    if debug: print indent(), 'trying', (r,c,n), 'nfilled', nfilled, 'sure' if sure else ''
+    mat[r][c] = n
+    incol[c].add(n)
+    inrow[r].add(n)
+    ingrp[grpof(r,c)].add(n)
+    res = rec(nfilled+1, sure and len(bestopts)==1)
+    mat[r][c] = None
+    incol[c].remove(n)
+    inrow[r].remove(n)
+    ingrp[grpof(r,c)].remove(n)
+    if res is not None: return res
+nfilled0 = sum(sum(1 if x is not None else 0 for x in row) for row in mat)
+mat = rec(nfilled0, True)
+print '\n'.join(''.join(' ' if c is None else str(c) for c in line) for line in mat)
 
 # vim: et sw=2 ts=2
